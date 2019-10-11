7fea5018728924d7d4dcb0e189b434b38467cce5
Neophear <stiiggade@gmail.com>
2019-10-11 10:41:44 +0200

    Initial commit

---
+++ ClientApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var t = client.GetStringAsync("https://localhost:44334/api/values");
+            Task.Run(() => t).Wait();
+
+            Console.WriteLine(t.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServiceProject/Middleware/AdminSafelistMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ServiceProject.Middleware
+{
+    public class AdminSafeListMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _adminSafeList;
+
+        public AdminSafeListMiddleware(RequestDelegate next, string adminSafeList)
+        {
+            _adminSafeList = adminSafeList;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var remoteIp = context.Connection.RemoteIpAddress;
+
+            string[] ip = _adminSafeList.Split(';');
+
+            var bytes = remoteIp.GetAddressBytes();
+            var badIp = true;
+            foreach (var address in ip)
+            {
+                var testIp = IPAddress.Parse(address);
+                if (testIp.GetAddressBytes().SequenceEqual(bytes))
+                {
+                    badIp = false;
+                    break;
+                }
+            }
+
+            if (badIp)
+            {
+                Debug.WriteLine($"Forbidden Request from Remote IP address: {remoteIp}");
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
